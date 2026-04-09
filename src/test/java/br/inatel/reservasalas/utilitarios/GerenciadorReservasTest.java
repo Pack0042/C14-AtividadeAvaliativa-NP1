050ffe266dd92bb2b82b65879018ab17a64aaf31
Pedro Andrade <phpa_psa@hotmail.com>
2026-04-09 03:18:48 -0300

    test: impede reserva em horario passado

---
+++ src/test/java/br/inatel/reservasalas/utilitarios/GerenciadorReservasTest.java
@@ -0,0 +1,26 @@
+package br.inatel.reservasalas.utilitarios;
+
+import br.inatel.reservasalas.entidades.Sala;
+import br.inatel.reservasalas.entidades.Usuario;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GerenciadorReservasTest {
+
+    @Test
+    void impedirReservaEmHorarioPassado() {
+        GerenciadorReservas gerenciadorReservas = new GerenciadorReservas();
+        Usuario usuario = new Usuario("Chris", "chris@email.com", "shampooantiquedasnaofunciona");
+        Sala sala = new Sala(101, "Sala de Aula", 30);
+        LocalDateTime horarioPassado = LocalDateTime.now().minusHours(1);
+
+        String resultado = gerenciadorReservas.criar(usuario, sala, horarioPassado);
+
+        assertEquals("Erro: nao e permitido reservar em horarios passados.", resultado);
+        assertTrue(gerenciadorReservas.listarReservas().isEmpty());
+    }
+}
